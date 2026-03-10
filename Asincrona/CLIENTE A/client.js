584343b3852dca15d1fd69668321e2eb957b9651
David Hernandez <dahe_22@alu.uabcs.mx>
2026-03-09 17:46:40 -0700

    Actualizar IP del servidor en la función de procesamiento de imágenes

--- Asincrona/CLIENTE A/client.js
+++ Asincrona/CLIENTE A/client.js
@@ -3,7 +3,7 @@ const axios = require('axios'); // [cite: 86]
 async function procesarImagen(imagen) { // [cite: 87]
     try { // [cite: 88]
         // OJO: Cambia IP_DEL_SERVIDOR por la IP real, por ejemplo: 192.168.1.75
-        const respuesta = await axios.post('http://192.168.1.77:3000/procesarImagen', { // [cite: 89]
+        const respuesta = await axios.post('http://192.168.1.90:3000/procesarImagen', { // [cite: 89]
             imagen // [cite: 89]
         }); // [cite: 90]
         console.log('Imagen procesada:', respuesta.data.imagenProcesada); // [cite: 91]
