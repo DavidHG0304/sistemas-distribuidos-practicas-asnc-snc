1cde0628b33e108d3bd895a824aa98c35a5cb363
David Hernandez <dahe_22@alu.uabcs.mx>
2026-03-09 17:43:12 -0700

    Cambios en el servidor de la practica asincrona

--- Asincrona/SERVIDOR A/server.js
+++ Asincrona/SERVIDOR A/server.js
@@ -7,9 +7,16 @@ app.use(express.json()); // [cite: 73]
 app.post('/procesarImagen', (req, res) => { // [cite: 74]
     const imagen = req.body.imagen; // [cite: 75, 76]
     
+    // LOG 1: Registra cuando llega la solicitud
+    console.log(`[SERVIDOR] Solicitud recibida. Procesando: ${imagen}...`);
+    
     // Simulación de procesamiento de imagen que tarda 2 segundos [cite: 77, 79]
     setTimeout(() => {
         const imagenProcesada = `Imagen procesada: ${imagen}`; // [cite: 78]
+        
+        // LOG 2: Registra cuando termina y envía la respuesta
+        console.log(`[SERVIDOR] Trabajo terminado. Enviando: ${imagenProcesada}`);
+        
         res.send({ imagenProcesada }); // [cite: 78]
     }, 2000); // [cite: 79]
 }); // [cite: 80]
